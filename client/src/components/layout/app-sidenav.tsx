c4e24618cf285036c109babc4139a9e25d289a8b
james1343 <43042832-james1343@users.noreply.replit.com>
2026-05-27 08:53:46 +0000

    Restored to '9b5196319eaaa3bd405f316af26ed508c056475d'
    Replit-Restored-To: 9b5196319eaaa3bd405f316af26ed508c056475d

--- client/src/components/layout/app-sidenav.tsx
+++ client/src/components/layout/app-sidenav.tsx
@@ -265,7 +265,7 @@ function SidenavInner({
             </div>
           </div>
         ) : currentOrganization?.logoUrl ? (
-          <div className="flex items-center justify-start -my-5">
+          <div className="flex items-center justify-start">
             <img
               src={currentOrganization.logoUrl}
               alt={currentOrganization.name || currentUser?.orgName || "Logo"}
